b87da55ff814a6cd75837a399954b92cdfcb6bc4
Jan Vraštil <vrastil.honza@seznam.cz>
2022-03-21 10:12:27 +0100

    changes after code review

--- lib/counter.dart
+++
@@ -1,9 +0,0 @@
-// redundant class for testing unit tests.
-// Feel free to remove it in the future (together with the unit tests)
-class Counter {
-  int value = 0;
-
-  void increment() => value++;
-
-  void decrement() => value--;
-}
