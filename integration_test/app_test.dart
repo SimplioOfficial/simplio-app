3dac5cffb26fff3ee6e35926c2ff73cc326e9415
Mariusz <mariusz.wluka@simplio.io>
2022-04-13 01:40:41 +0200

    Applying changes after update of initialInfrastructure branch

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,31 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
 import 'package:simplio_app/main.dart' as app;
 
 void main() {
+
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('end-to-end test', () {
-    testWidgets('tap on the floating action button, verify counter',
+    testWidgets('verify import seed tab',
             (WidgetTester tester) async {
           app.main();
           await tester.pumpAndSettle();
 
-          // Verify the counter starts at 0.
-          expect(find.text('0'), findsOneWidget);
-
-          // Finds the floating action button to tap on.
-          final Finder fab = find.byTooltip('Increment');
-
-          // Emulate a tap on the floating action button.
-          await tester.tap(fab);
-
-          // Trigger a frame.
-          await tester.pumpAndSettle();
-
-          // Verify the counter increments by 1.
-          expect(find.text('1'), findsOneWidget);
-        });
+          //Verify if Seed title is present
+          expect(find.text('Seed'), findsOneWidget);
+          expect(find.text('Generated seed'), findsOneWidget);
+         });
   });
 }
