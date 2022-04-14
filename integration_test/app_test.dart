16a38e5791b139e34a7b9cc553567e1499a95452
Juliana Souza <juliana.souza@simplio.io>
2022-04-14 20:02:36 -0300

    add integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,21 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:simplio_app/main.dart' as app;
-
+import 'package:simplio_app/main.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('end-to-end test', () {
-    testWidgets('tap on the floating action button, verify counter',
-            (WidgetTester tester) async {
-          app.main();
-          await tester.pumpAndSettle();
+  group('Testing App Performance Tests', () {
+    testWidgets('add wallet', (tester) async {
+      await tester.pumpAndSettle();
+      await tester.pumpWidget(const SimplioApp());
+      const switchKey = 'switch_widget';
 
-          //Verify if Seed title is present
-          expect(find.text('Seed'), findsOneWidget);
-          expect(find.text('Generated seed'), findsOneWidget);
-         });
+      await tester.tap(find.text('Next'));
+      await tester.pumpAndSettle();
+      expect(find.text('You have no wallet'), findsOneWidget);
+
+      await tester.pumpAndSettle();
+      await tester.tap(find.byIcon(Icons.add));
+
+      await tester.pumpAndSettle();
+      expect(find.byKey(const ValueKey(switchKey)), findsWidgets);
+      await tester.pumpAndSettle();
+      //error for tap switch
+      //  await tester.tap(find.byKey(const ValueKey(switchKey)));
     });
+  });
 }
