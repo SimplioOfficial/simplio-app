8c06746d0412b59606059e92c50c1ca42447c6f9
Filip Storkan <filip.storkan@gmail.com>
2022-05-04 09:48:19 +0200

    reducing layout rebuilds

--- lib/data/model/asset.dart
+++ lib/data/model/asset.dart
@@ -4,20 +4,49 @@ import 'package:flutter/material.dart';
 class Asset extends Equatable {
   final String name;
   final String ticker;
-  final Color primaryColor;
-  final Color foregroundColor;
-  final IconData icon;
+  final AssetStyle style;
   final List<Asset>? networks;
 
   const Asset({
     required this.name,
     required this.ticker,
-    required this.primaryColor,
-    required this.foregroundColor,
-    required this.icon,
+    required this.style,
     this.networks,
   });
 
   @override
-  List<Object?> get props => [name, ticker, primaryColor, networks];
+  List<Object?> get props => [
+        name,
+        ticker,
+        style,
+        networks,
+      ];
+}
+
+class AssetStyle {
+  final Color primaryColor;
+  final Color foregroundColor;
+  final IconData icon;
+
+  const AssetStyle({
+    required this.icon,
+    required this.primaryColor,
+    required this.foregroundColor,
+  });
+}
+
+class AssetToggle extends Equatable {
+  final bool toggled;
+  final Asset asset;
+
+  const AssetToggle({
+    this.toggled = false,
+    required this.asset,
+  });
+
+  @override
+  List<Object> get props => [
+        toggled,
+        asset,
+      ];
 }
