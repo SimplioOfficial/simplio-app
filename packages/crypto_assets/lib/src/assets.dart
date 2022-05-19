2ca48c463f0912e14b44ffa8166765393a8cfe5c
Filip Storkan <filip.storkan@gmail.com>
2022-05-19 18:08:41 +0200

    adding asset's decimal

--- packages/crypto_assets/lib/src/assets.dart
+++ packages/crypto_assets/lib/src/assets.dart
@@ -22,83 +22,137 @@ class Assets {
     "simplio": Asset(
       detail: AssetDetails.simplio,
       assetTypes: [
-        TokenAsset(contractAddress: '', network: Networks.solana),
+        TokenAsset(
+          contractAddress: '',
+          network: Networks.solana,
+          decimal: 10,
+        ),
       ],
     ),
     "bitcoin": Asset(
       detail: AssetDetails.bitcoin,
       assetTypes: [
-        NativeAsset(network: Networks.bitcoin),
+        NativeAsset(
+          network: Networks.bitcoin,
+          decimal: 10,
+        ),
       ],
     ),
     "ethereum": Asset(
       detail: AssetDetails.ethereum,
       assetTypes: [
-        NativeAsset(network: Networks.ethereum),
+        NativeAsset(
+          network: Networks.ethereum,
+          decimal: 10,
+        ),
       ],
     ),
     "solana": Asset(
       detail: AssetDetails.solana,
       assetTypes: [
-        NativeAsset(network: Networks.solana),
+        NativeAsset(
+          network: Networks.solana,
+          decimal: 10,
+        ),
       ],
     ),
     "bitcoin-cash": Asset(
       detail: AssetDetails.bitcoinCash,
       assetTypes: [
-        NativeAsset(network: Networks.bitcoinCash),
+        NativeAsset(
+          network: Networks.bitcoinCash,
+          decimal: 10,
+        ),
       ],
     ),
     "dogecoin": Asset(
       detail: AssetDetails.dogecoin,
       enabled: false,
       assetTypes: [
-        NativeAsset(network: Networks.dogecoin),
+        NativeAsset(
+          network: Networks.dogecoin,
+          decimal: 10,
+        ),
       ],
     ),
     "dash": Asset(
       detail: AssetDetails.dash,
       assetTypes: [
-        NativeAsset(network: Networks.dash),
+        NativeAsset(
+          network: Networks.dash,
+          decimal: 10,
+        ),
       ],
     ),
     "digibyte": Asset(
       detail: AssetDetails.digibyte,
       assetTypes: [
-        NativeAsset(network: Networks.digibyte),
+        NativeAsset(
+          network: Networks.digibyte,
+          decimal: 10,
+        ),
       ],
     ),
     "litecoin": Asset(
       detail: AssetDetails.litecoin,
       assetTypes: [
-        NativeAsset(network: Networks.litecoin),
+        NativeAsset(
+          network: Networks.litecoin,
+          decimal: 10,
+        ),
       ],
     ),
     "zcash": Asset(
       detail: AssetDetails.zcash,
       assetTypes: [
-        NativeAsset(network: Networks.zcash),
+        NativeAsset(
+          network: Networks.zcash,
+          decimal: 10,
+        ),
       ],
     ),
     "flux": Asset(
       detail: AssetDetails.flux,
       assetTypes: [
-        NativeAsset(network: Networks.flux),
-        TokenAsset(contractAddress: '', network: Networks.ethereum),
+        NativeAsset(
+          network: Networks.flux,
+          decimal: 10,
+        ),
+        TokenAsset(
+          contractAddress: '',
+          network: Networks.ethereum,
+          decimal: 10,
+        ),
       ],
     ),
     "binance-usd": Asset(
       detail: AssetDetails.binanceUSD,
       assetTypes: [
-        TokenAsset(contractAddress: '', network: Networks.binance),
+        TokenAsset(
+          contractAddress: '',
+          network: Networks.binance,
+          decimal: 10,
+        ),
       ],
     ),
     "chainlink": Asset(
       detail: AssetDetails.chainlink,
       assetTypes: [
-        TokenAsset(contractAddress: '', network: Networks.solana),
-        TokenAsset(contractAddress: '', network: Networks.ethereum),
-        TokenAsset(contractAddress: '', network: Networks.binance),
+        TokenAsset(
+          contractAddress: '',
+          network: Networks.solana,
+          decimal: 10,
+        ),
+        TokenAsset(
+          contractAddress: '',
+          network: Networks.ethereum,
+          decimal: 10,
+        ),
+        TokenAsset(
+          contractAddress: '',
+          network: Networks.binance,
+          decimal: 10,
+        ),
       ],
     ),
   };
