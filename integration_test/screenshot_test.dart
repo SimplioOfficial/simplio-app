5e5fcc604920d5eb3d3713ea745cd082966aee25
Juliana Souza <juliana.souza@simplio.io>
2022-05-23 16:47:21 -0300

    screenshot test2

--- integration_test/screenshot_test.dart
+++ integration_test/screenshot_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:simplio_app/main.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized()
@@ -11,17 +12,26 @@ void main() {
   testWidgets('screenshot', (WidgetTester tester) async {
     // Build the app.
     const importRecoverySeedKey = 'manual_seed_import';
+    String platformName = '';
     // This is required prior to taking the screenshot (Android only).
-    if (Platform.isAndroid) {
+    if (!kIsWeb) {
       await binding.convertFlutterSurfaceToImage();
+
+      if (Platform.isAndroid) {
+        platformName = "android";
+      } else {
+        platformName = "ios";
+      }
+    } else {
+      platformName = "web";
     }
     await tester.pumpWidget(const SimplioApp());
     // Trigger a frame.
     await tester.pumpAndSettle();
-    await binding.takeScreenshot('screenshot-1');
+    await binding.takeScreenshot('1-screenshot-$platformName');
 
     await tester.tap(find.byKey(const ValueKey(importRecoverySeedKey)));
     await tester.pumpAndSettle();
-    await binding.takeScreenshot('screenshot-2');
+    await binding.takeScreenshot('2-screenshot-$platformName');
   });
 }
