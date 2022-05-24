ac3021df64258b20c171c2cbd0e5135801111e2b
Juliana Souza <juliana.souza@simplio.io>
2022-05-24 15:25:18 -0300

    screenshot test4

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -4,7 +4,9 @@ import 'package:integration_test/integration_test_driver_extended.dart';
 Future<void> main() async {
   await integrationDriver(
     onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
-      final File image = await File('/tmp/screenshots/$screenshotName.png');
+      final File image = await File('/tmp/screenshots/$screenshotName.png')
+          .create(recursive: true);
+
       image.writeAsBytesSync(screenshotBytes);
       // Return false if the screenshot is invalid.
       return true;
