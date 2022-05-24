57248332f4b15abea3b185d91b2e0c2deb8aab21
Filip Storkan <filip.storkan@gmail.com>
2022-05-24 11:52:24 +0200

    fixing type

--- lib/logic/asset_toggle_cubit/asset_toggle_cubit.dart
+++ lib/logic/asset_toggle_cubit/asset_toggle_cubit.dart
@@ -1,13 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:crypto_assets/crypto_assets.dart';
 import 'package:equatable/equatable.dart';
+import 'package:simplio_app/data/model/asset_wallet.dart';
 
 part 'asset_toggle_state.dart';
 
 class AssetToggleCubit extends Cubit<AssetToggleState> {
   AssetToggleCubit() : super(const AssetToggleState());
 
-  List<AssetToggle> loadToggles(List<Asset> assets, List<Asset> enabled) {
+  List<AssetToggle> loadToggles(List<Asset> assets, List<AssetWallet> enabled) {
     final toggles = assets
         .map((a) => AssetToggle(
               asset: a,
